75c5d747bd0dcd1572916b6ab9c9774a5f4d37c5
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2020-09-26 03:40:41 +0300

    Create Connected.py

---
+++ Connected.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#x = 1=0
+
+x = "4+4=8+8=16+16=32=0"
+x1 = "Angels on the sideline Puzzled and amused Why did Father give these humans free will? Now they re all confused"
+x2 = "People are willing to change their belief system so that they buy cheaper oil"
+x3 = "I'm accelerator"
+x4 = "-+-=0"
+x5 = "-*-*-=0"
+x6 = "I was born for multithreading born to the stars I was born to dream"
+try_to_keep_it_simple = "True"
+do = "or do not, there is no try"
+
+print("False")
+print(bool(x1))
+print("False")
+print(bool(x3))
+print(bool(x4))
+print(bool(x5))
+print(bool(x6))
+print(bool(try_to_keep_it_simple))
+print("I am connected therefore I am\n"*10)
